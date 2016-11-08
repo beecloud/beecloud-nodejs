cd3971d8a8600526a741c3dae4deb1dc3203d42c
undefined <undefined@bogon>
2016-11-08 20:11:15 +0800

    sdk文件夹移到根目录

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const BCRESTAPI = require('./sdk');
+const BCRESTAPI = require('../sdk');
 // const BCRESTAPI = require('beecloud-node-sdk');
 const config = require('./config');
 
